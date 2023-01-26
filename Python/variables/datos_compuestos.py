a29085c4d7dada87a920885932ac6d4075a26b0b
felipelens <uruena271@gmail.com>
2023-01-26 12:37:05 -0500

    se arreglo la carpeta python

---
+++ Python/variables/datos_compuestos.py
@@ -0,0 +1,4 @@
+# lista  
+#                 0              1              2
+lista = ["cristian felipe","aprendo python","21 anos"]
+print(lista[1])
---
+++ Python/variables/variables.py
@@ -0,0 +1,21 @@
+# declarar variable
+nombre = "cristian "
+apellido = "lenis"
+edad = 21
+
+# imprimir y concatenar con +
+print(nombre+apellido)
+
+# eliminar variable
+del apellido
+
+# concatenar con f a string
+print(nombre + f"{edad}")
+
+# forma correcta de asignar nombres a variables
+nombre_completo = "cristian felipe lenis"
+
+# para saber si existe un texto en una variable
+print("felipe" in nombre_completo) # con in es para saber si esta
+print("bulla" not in nombre_completo) # con not in es para saber si no esta
+
