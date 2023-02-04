ad203fe3c302eb83986e70579402e86470c5c3fe
felipelens <uruena271@gmail.com>
2023-02-04 11:53:07 -0500

    informacion reciente 4/02/2023

---
+++ Python/variables/2 datos_compuestos.py
@@ -0,0 +1,25 @@
+# lista  
+#                 0              1              2
+lista = ["cristian felipe","aprendo python","21 anos"]
+
+# tupla no se puede modificar
+tupla = ("cristian felipe","aprendo python","21 anos")
+#tupla[3] = "21 anos" <- no se puede hacer
+
+# creando un conjunto no se pueden llamar los elementos por indice y no muestra datos duplicados
+conjunto = {"cristian felipe","aprendo python","21 anos"}
+
+#diccionario 
+nombres =  {
+    'cristian': 'felipe',
+    'edad':21,
+    'localidad':'honda'
+    }
+
+print(conjunto)
+
+
+
+
+
+
--- Python/variables/datos_compuestos.py
+++
@@ -1,4 +0,0 @@
-# lista  
-#                 0              1              2
-lista = ["cristian felipe","aprendo python","21 anos"]
-print(lista[1])
