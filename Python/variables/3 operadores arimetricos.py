d05947c84a9dc04cb532ba100f601568ad9e2672
felipelens <uruena271@gmail.com>
2023-02-10 12:21:11 -0500

    se agregaron operadores logicos

---
+++ Python/variables/3 operadores arimetricos.py
@@ -0,0 +1,13 @@
+num1 = 1
+num2 = 2
+
+
+num = num1 + num2
+num = num1 * num2
+num = num1 - num2
+num = num1 / num2
+num = num1 // num2 # suma baja sirve para quitar decimales hacia abajo *FLOTANTE*
+num = num1 % num2  # restante de una division  *FLOTANTE*
+
+
+
---
+++ Python/variables/5 metodos.py
@@ -0,0 +1,4 @@
+num = 1
+# type() #* funciona para saber que tipo de dato se esta ingresando
+# dir(num) #* funciona para saber que podemos hacer con ese tipo de dato
+print()
