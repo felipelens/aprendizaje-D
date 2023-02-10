2b7d931861ad3538a18ae0ea6322d7648cdaf66b
felipelens <uruena271@gmail.com>
2023-02-10 13:05:32 -0500

    no se prron probando

--- Python/variables/5 metodos.py
+++ Python/variables/5 metodos.py
@@ -1,4 +1,58 @@
 num = 1
-# type() #* funciona para saber que tipo de dato se esta ingresando
-# dir(num) #* funciona para saber que podemos hacer con ese tipo de dato
-print()
+nombre = "Cristian Felipe"
+prueba = "32132"
+prueba2 = "aasdfasdf123456789"
+#! Funciones
+# type(variable) #* funciona para saber que tipo de dato se esta ingresando
+# dir(variable) #* funciona para saber que podemos hacer con ese tipo de dato
+# len(variable) #* funciona para saber cuantos caracteres hay en una varriable
+#! Funciones
+
+
+#! Metodos
+# es una funcion aplicada a un objeto
+# para poner en mayusculas
+result = nombre.upper() 
+
+# para poner en minusculas
+result = nombre.lower() 
+
+# primera letra en mayusculas
+result = nombre.capitalize() 
+
+# para buscar un string en alguna cadena y que devuelva la posicion en numero
+result = nombre.find("e") 
+
+# index funciona igual que find() pero si no encuentra la letra a buscar depura una exepcion
+result = nombre.index("e")
+
+# si es numerico devuelve true
+result = prueba.isnumeric()
+
+# si es alfanumerico devuelve true a hasta z
+result = prueba2.isalpha()
+
+
+# count funciona para saber cuantos letras estan repetidas en una variable
+# si no encuentra nada devuelve 0
+result = nombre.count("e")
+
+# si empieza con " " devuelve true
+result = nombre.startswith("Cri")
+
+# si termina con " " devuelve true
+result = nombre.endswith("e")
+
+#reemplaza dentro de una cadena
+#si encuentra la coincidencia
+result = nombre.replace("Felipe","lenis")
+
+
+
+#! Metodos
+
+print(result)
+
+
+
+
